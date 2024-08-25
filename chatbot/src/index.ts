790ea69e66d5e46e39ce90638abe9f16d3d45e75
naomiHauret <hauretnaomi@gmail.com>
2024-08-25 14:40:12 +0200

    feat: test - render frame with messagekit

--- chatbot/src/index.ts
+++ chatbot/src/index.ts
@@ -1,9 +1,16 @@
 import { run, HandlerContext } from '@xmtp/message-kit'
+import { tunnelmole } from 'tunnelmole'
+
+const url = process.env.NODE_ENV !== 'production' ? await tunnelmole({
+  port: 5173
+}) : process.env.FRAME_URL
+
+console.log(`Frame running  at ${url} ; open ${url}/dev to debug`)
 
 run(async (context: HandlerContext) => {
   // Get the message and the address from the sender
   const { content, sender } = context.message
 
   // To reply, just call `reply` on the HandlerContext.
-  await context.reply(`gm`)
+  await context.reply(url)
 })
